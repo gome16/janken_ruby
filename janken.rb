c4237a1fcb1e532b9e04e7ea855757267256c948
EC2 Default User <ec2-user@ip-172-31-31-117.ap-northeast-1.compute.internal>
2023-02-07 01:09:30 +0000

    RUBYあっち向いてホイ修正

--- janken.rb
+++ janken.rb
@@ -1,96 +1,78 @@
-class Number
-  def janken_selector(number)
-    @number = number
+class Janken_game
+  def first_game
+    while true
+      puts "0 → グー / 1 → チョキ / 2 → パー"
+      @janken_hand = gets.to_i
+      @janken_computer = rand(0..2)
+      @select_hand = ["グー","チョキ","パー"]
+      puts "あなたは#{@select_hand[@janken_hand]}"
+      puts "pcは#{@select_hand[@janken_computer]}"
+
+      if @janken_hand == @janken_computer
+        puts "あいこ"
+
+      elsif (@janken_hand == 0 && @janken_computer == 1) || (@janken_hand == 1 && @janken_computer == 2) || (@janken_hand == 2 && @janken_computer == 0)
+        return 1
+        
+      elsif (@janken_hand == 0 && @janken_computer == 2) || (@janken_hand == 1 && @janken_computer == 0) || (@janken_hand == 2 && @janken_computer == 1)
+        return 2
+        
+      else
+        puts "番号外指定"
+      end
+
+    end   
   end
 
-  def deflection_selector(number)
-    @number = number
-  end
-end
-
-number_selector = Number.new
-janken_rand = Random.new.rand(1..3)
-deflection_rand = Random.new.rand(1..4)
-
-puts "1 → グー / 2 → チョキ / 3 → パー"
-case number_selector.janken_selector(gets.to_i)
-when 1 #ユーザーグーパターン
-  puts "あなた　→　グー"
-  case janken_rand
-  when 1
-    puts "相手　→ グー"
-    puts "あいこ"
-
-  when 2
-    puts "相手 →　チョキ"
-    puts "あなたが指定した方向に相手が向いたら勝ちです"
-    puts "方向指定 上 → 1 / 右 → 2 / 下 → 3 / 左 → 4"
-    if number_selector.deflection_selector(gets.to_i) == deflection_rand 
-      puts "勝ち"
-    else
-      puts "引き分け"
-    end
-
-  when 3 
-    puts "相手　→ パー"
-    puts "相手が指定した方向にあなたが向いたら負けです"
-    puts "方向指定 上 → 1 / 右 → 2 / 下 → 3 / 左 → 4"
-    if number_selector.deflection_selector(gets.to_i) == deflection_rand
-      puts "負け"
-    else
-      puts "引き分け"
-    end
+  def def_select
+    puts "方向指定 上 → 0 / 右 → 1 / 下 → 2 / 左 → 3"
+    @def_hand = ["上","右","下","左"]
+    @def_user = gets.to_i
+    @def_computer = rand(0..3)
+    puts "あなたは#{@def_hand[@def_user]}"
+    puts "pcは#{@def_hand[@def_computer]}"
   end
 
-when 2 #ユーザーチョキパターン
-  case janken_rand
-  when 1
-    puts "相手　→ パー"
-    puts "相手が指定した方向にあなたが向いたら負けです"
-    puts "方向指定 上 → 1 / 右 → 2 / 下 → 3 / 左 → 4"
-    if number_selector.deflection_selector(gets.to_i) == deflection_rand
-      puts "負け"
-    else
-      puts "引き分け"
-    end
+  def secnd_game
+    case first_game
+    when 1
+      puts "ジャンケン勝ち"
+      puts "あなたが指定した方向にpcが向いたら勝ち"
 
-  when 2
-    puts "相手　→ チョキ"
-    puts "あいこ"
+      def_select
+      if @def_user == @def_computer
+        puts "あなたの勝ち"
+        return 3
 
-  when 3
-    puts "相手 →　パー"
-    puts "あなたが指定した方向に相手が向いたら勝ちです"
-    puts "方向指定 上 → 1 / 右 → 2 / 下 → 3 / 左 → 4"
-    if number_selector.deflection_selector(gets.to_i) == deflection_rand 
-      puts "勝ち"
-    else
-      puts "引き分け"
+      else
+        puts "じゃんけんからやり直し"
+      end
+
+    when 2
+      puts "ジャンケン負け"
+      puts "pcが指定した方向にあなたが向いたら負け"
+      
+      def_select
+      if @def_user == @def_computer
+        puts "あなたの負け"
+        return 3
+      
+      else
+        puts "じゃんけんからやり直し"
+      end
     end
   end
-
-when 3 #ユーザーパーパターン
-  case janken_rand
-  when 1
-    puts "相手 → グー"
-    puts "あなたが指定した方向に相手が向いたら勝ちです"
-    puts "方向指定 上 → 1 / 右 → 2 / 下 → 3 / 左 → 4"
-    if number_selector.deflection_selector(gets.to_i) == deflection_rand 
-      puts "勝ち"
-    else
-      puts "引き分け"
+  
+  def game_control
+    while true
+      if secnd_game == 3
+        break
+      end
     end
-  when 2
-    puts "相手　→ チョキ"
-    puts "相手が指定した方向に相手が向いたら負けです"
-    puts "方向指定 上 → 1 / 右 → 2 / 下 → 3 / 左 → 4"
-    if number_selector.deflection_selector(gets.to_i) == deflection_rand
-      puts "負け"
-    else
-      puts "引き分け"
-    end
-  when 3
-    puts "相手　→ パー"
-    puts "あいこ"
   end
 end
+
+
+
+janken = Janken_game.new()
+janken.game_control()
